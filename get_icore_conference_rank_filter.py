50d734167259d7af0318d046021b22816b1e7956
Lê Minh Hoàng <75823146+leminhhoang04@users.noreply.github.com>
2025-06-05 16:08:55 +0700

    Update get_icore_conference_rank_filter.py

--- get_icore_conference_rank_filter.py
+++ get_icore_conference_rank_filter.py
@@ -4,5 +4,5 @@ df = pd.read_csv("icore-conf-rank.csv", encoding="utf-8")
 print(df['rank'].value_counts())
 
 df_filtered = df[df['rank'].isin(['A', 'A*'])]
-df_filtered.to_csv("icore-conf-rank-A-A*.csv", index=False, encoding="utf-8")
+df_filtered.to_csv("icore-conf-rank-A-Astar.csv", index=False, encoding="utf-8")
 print(df_filtered['rank'].value_counts())
